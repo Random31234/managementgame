b3ccb43392a9a12ed5a5da045362eba1252be903
Random31234 <randorte3@gmail.com>
2025-09-29 09:10:06 -0700

    Initial commit

---
+++ KoboldPlacements.gd
@@ -0,0 +1,5 @@
+extends Node
+
+@export var tavernWorkers:Array[kobold]
+@export var unusedkobolds:Array[kobold]
+@export var guards:Array[kobold]
---
+++ Scripts/Resources script/KoboldBase.gd
@@ -0,0 +1,33 @@
+extends Resource
+
+class_name kobold
+
+@export var n:String
+
+@export var mainColor:Color
+@export var secondaryColor:Color
+@export var eyeColor:Color
+
+@export var sex:bool
+
+#basic stats
+@export var str:Big
+@export var dex:Big
+@export var con:Big
+@export var wis:Big
+@export var cha:Big
+@export var i:Big
+#the bars, set them up later
+@export var hp:Big
+@export var san:Big
+@export var curHp:Big
+@export var curSan:Big
+#weight
+@export var weight:Big
+
+@export var strG:Big
+@export var dexG:Big
+@export var conG:Big
+@export var wisG:Big
+@export var chaG:Big
+@export var iG:Big
